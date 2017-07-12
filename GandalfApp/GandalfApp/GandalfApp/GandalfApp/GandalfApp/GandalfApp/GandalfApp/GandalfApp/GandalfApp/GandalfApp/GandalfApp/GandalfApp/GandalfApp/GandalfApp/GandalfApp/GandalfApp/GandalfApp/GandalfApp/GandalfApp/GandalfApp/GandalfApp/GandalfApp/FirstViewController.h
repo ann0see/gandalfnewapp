b5b650c0bfb07ef45709711be3653ba8213abed9
jonisc <bananianx@t-online.de>
2017-07-12 18:19:58 +0200

    First commit

---
+++ GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  GandalfApp
+//
+//  Created by Jonathan ***REMOVED*** on 12.07.17.
+//  Copyright © 2017 Gandalf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  GandalfApp
+//
+//  Created by Jonathan ***REMOVED*** on 12.07.17.
+//  Copyright © 2017 Gandalf. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
