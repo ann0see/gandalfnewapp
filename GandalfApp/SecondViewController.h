eeb16cf15d42fb80b65f39397899004855f356b3
jonisc <bananianx@t-online.de>
2017-07-12 15:49:44 +0200

    Initial Commit

---
+++ GandalfApp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  GandalfApp
+//
+//  Created by ***REMOVED*** on 12.07.17.
+//  Copyright © 2017 Gandalf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
