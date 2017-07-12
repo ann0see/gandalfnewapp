ffd13ae15772b0f896e6383e621d336d5be9ba24
jonisc <bananianx@t-online.de>
2017-07-12 15:49:44 +0200

    Initial Commit

---
+++ GandalfApp/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  GandalfApp
+//
+//  Created by Jonathan Smith-Chung on 12.07.17.
+//  Copyright © 2017 Gandalf. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
