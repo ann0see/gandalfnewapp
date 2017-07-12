b129e2b9390bb451ff40b36035dce4ea1c69f445
jonisc <bananianx@t-online.de>
2017-07-12 18:19:58 +0200

    First commit

---
+++ GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  GandalfApp
+//
+//  Created by Jonathan Smith-Chung on 12.07.17.
+//  Copyright © 2017 Gandalf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/GandalfApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GandalfApp
+//
+//  Created by Jonathan Smith-Chung on 12.07.17.
+//  Copyright © 2017 Gandalf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
