75394fe4b4376f62ea4094ec22cdbda1167198e7
jonisc <bananianx@t-online.de>
2017-08-06 12:46:51 +0200

    Checking if app is running as root

--- GandalfApp/SecondViewController.m
+++ GandalfApp/SecondViewController.m
@@ -17,6 +17,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if (getuid() != 0 ) // Check if running not as root and give error
+    {
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Not running as root"
+            message:@"This app is currently not running as root user. Actions in this view will most likely fail. If you think this is an error, please contact us."
+            preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){}]; // Dismiss button
+        UIAlertAction* contactAction = [UIAlertAction actionWithTitle:@"Contact" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
+        {
+            NSURL *contactUrl = [NSURL URLWithString:@"https://github.com/ethanrdoesmc/gandalf/issues"];
+            if (![[UIApplication sharedApplication] openURL:contactUrl]) {
+                NSLog(@"%@%@",@"Failed to open url:",[contactUrl description]); // If failed goes here
+        }
+        }]; //Contact Button; Opens issues tab on GitHub repo
+
+        [alert addAction:defaultAction];
+        [alert addAction:contactAction];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
