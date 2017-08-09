3003a42890cb316945012c8f51a62197225a89e6
jonisc <bananianx@t-online.de>
2017-08-09 21:33:26 +0200

    Checking if posix_spawn exited correctly

--- GandalfApp/SecondViewController.m
+++ GandalfApp/SecondViewController.m
@@ -45,18 +45,49 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)holdButton:(id)sender {
-    if (getuid() != 0) {
+    /*if (getuid() != 0) {
         UIButton *button = (UIButton *)sender;
         button.enabled = NO;
         [button setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
-        [button setTitle:@"ERROR" forState:UIControlStateNormal];
-    }
+        [button setTitle:@"FNO ROOT ERROR" forState:UIControlStateNormal];
+    }*/
+    
     pid_t pid;
-    int status;
+    // int status;
     const char* args[] = {"gandalf", "hold", NULL};
-    posix_spawn(&pid, "/usr/bin/gandalf", NULL, NULL, (char* const*)args, NULL);
-    waitpid(pid, &status, WEXITED);//wait untill the process completes (only if you need to do that) (yes, we need that)
-    // system("/usr/bin/gandalf hold"); // execute gandalf hold command //is deprecated
+    int gafExitCode = posix_spawn(&pid, "/usr/bin/gandalf", NULL, NULL, (char* const*)args, NULL);
+    
+    
+    if (gafExitCode == 0) {
+        UIButton *button = (UIButton *)sender;
+        button.enabled = NO;
+        [button setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
+        [button setTitle:@"SUCCESS" forState:UIControlStateNormal];
+    }
+    else {
+        UIButton *button = (UIButton *)sender;
+        button.enabled = NO;
+        [button setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
+        [button setTitle:@"ERROR" forState:UIControlStateNormal]; //Make button show "ERROR"
+        
+        
+        NSString* messageString = [NSString stringWithFormat: @"Something went wrong. Please contact us.\nDebug: posix_spawn exit code: \"%i", gafExitCode];
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error" message:messageString preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){}]; // Dismiss button
+        UIAlertAction* contactAction = [UIAlertAction actionWithTitle:@"Contact" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
+                                        {
+                                            NSURL *contactUrl = [NSURL URLWithString:@"https://github.com/ethanrdoesmc/gandalf/issues"];
+                                            if (![[UIApplication sharedApplication] openURL:contactUrl]) {
+                                                NSLog(@"%@%@",@"Failed to open url:",[contactUrl description]); // If failed goes here
+                                            }
+                                        }]; //Contact Button; Opens issues tab on GitHub repo
+        
+        [alert addAction:defaultAction];
+        [alert addAction:contactAction];
+        [self presentViewController:alert animated:YES completion:nil]; // Show message if fails
+    }
+    
+    // waitpid(pid, &status, WEXITED); //wait until the process completes (only if you need to do that) (yes, we need that)
 }
 
 
