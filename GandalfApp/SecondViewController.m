13342a97184eac4c6c5c710544bd1389b776df2f
jonisc <bananianx@t-online.de>
2017-08-11 21:24:38 +0200

    First step for updater

--- GandalfApp/SecondViewController.m
+++ GandalfApp/SecondViewController.m
@@ -89,6 +89,77 @@
     
     // waitpid(pid, &status, WEXITED); //wait until the process completes (only if you need to do that) (yes, we need that)
 }
+// Check 4 updates
+- (IBAction)updateButton:(id)sender {
+    __block NSString *instVersion;
+    __block NSString *onlineVersion;
+    NSString *gandalfIdentifier = @"io.github.ethanrdoesmc.gandalf102"; //Bundle Identifier
+    // Get latest version from repository
+    
+    NSString *Url = @"https://ethanrdoesmc.github.io/gandalf/Packages";
+    NSMutableURLRequest *fileRequest = [[NSMutableURLRequest alloc] init];
+    [fileRequest setHTTPMethod:@"GET"];
+    [fileRequest setURL:[NSURL URLWithString:Url]];
+    
+    [[[NSURLSession sharedSession] dataTaskWithRequest:fileRequest completionHandler:
+      ^(NSData * _Nullable data,
+        NSURLResponse * _Nullable response,
+        NSError * _Nullable error) {
+          // we should check if response was 200 (success)
+          
+          NSString *packagesFile = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+          NSArray *packagesFileArray = [packagesFile componentsSeparatedByString:@"\n"]; // Save it in an array
+          NSString *rawVersion;
+          
+          // get all the properties of the gandalf Version from the array
+          NSString *packageLine = [NSString stringWithFormat:@"Package: %@", gandalfIdentifier]; // merge the 2 strings
+          NSUInteger indexOfGandalfBundleIdentifier = [packagesFileArray indexOfObject:packageLine]; //get the index of them
+          NSUInteger arrayCounter = indexOfGandalfBundleIdentifier; //Set where it should start searching
+          NSLog(@"Line: %lu", (unsigned long)indexOfGandalfBundleIdentifier); //output it to the log
+          do {
+               NSString *arrayLine = packagesFileArray[arrayCounter]; // Save content of array line...
+              if ([arrayLine containsString:(@"Version:")]){ //And check if it contains Version:
+                  NSLog(@"L: %@", arrayLine);
+                  rawVersion = [arrayLine stringByReplacingOccurrencesOfString:@"Version: " withString:@""]; // Remove Version: from this string
+                  onlineVersion = rawVersion; //save it to onlineVersion
+                  NSLog(@"%@", rawVersion);
+                  break; //Now it´s done, so we can stop the loop
+              }
+              arrayCounter++;
+          }
+          while (TRUE);
+          
+          // let´s get the content of /var/lib/dpkg/status and search for gandalfIdentifier
+          NSString *filepath = [[NSBundle mainBundle] pathForResource:@"/var/lib/dpkg/status" ofType:@"txt"];
+          NSError *errordpkg;
+          NSString *fileContents = [NSString stringWithContentsOfFile:filepath encoding:NSUTF8StringEncoding error:&error];
+          
+          if (error)
+              NSLog(@"Error reading file: %@", errordpkg.localizedDescription);
+          
+          // maybe for debugging...
+          NSLog(@"contents: %@", fileContents);
+          NSArray *dpkgStatusArray = [fileContents componentsSeparatedByString:@"\n"];
+
+          packageLine = [NSString stringWithFormat:@"Package: %@", gandalfIdentifier]; // merge the 2 strings
+          NSUInteger dpkgIndexOfGandalfBundleIdentifier = [dpkgStatusArray indexOfObject:packageLine]; //get the index of them
+          arrayCounter = dpkgIndexOfGandalfBundleIdentifier; //Set where it should start searching
+          do {
+              NSString *arrayLine = dpkgStatusArray[arrayCounter]; // Save content of array line...
+              if ([arrayLine containsString:(@"Version:")]){ //And check if it contains Version:
+                  NSLog(@"L: %@", arrayLine);
+                  rawVersion = [arrayLine stringByReplacingOccurrencesOfString:@"Version: " withString:@""]; // Remove Version: from this string
+                  instVersion = rawVersion; //save it to instVersion
+                  NSLog(@"%@", rawVersion);
+                  break; //Now it´s done, so we can stop the loop
+              }
+              arrayCounter++;
+          } while (TRUE);
+
+      }] resume];
+
+}
+
 
 
 @end
