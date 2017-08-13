df6d1d4b4e350a32a16b2ef71a747f6308e0bd52
jonisc <bananianx@t-online.de>
2017-08-13 20:52:30 +0200

    Update: now working with properties file

--- GandalfApp/SecondViewController.m
+++ GandalfApp/SecondViewController.m
@@ -7,8 +7,7 @@
 //
 
 #import "SecondViewController.h"
-
-
+#import "DDFileReader.h"
 @interface SecondViewController ()
 
 
@@ -20,6 +19,7 @@
     [super viewDidLoad];
     if (getuid() != 0 ) // Check if running not as root and give error
     {
+        NSLog(@"App is not running as root!");
         UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Not running as root"
                                                                        message:@"This app is currently not running as root user. Actions in this view will most likely fail. If you think this is an error, please contact us."
                                                                 preferredStyle:UIAlertControllerStyleAlert];
@@ -72,7 +72,7 @@
         [button setTitle:@"ERROR" forState:UIControlStateNormal]; //Make button show "ERROR"
         
         
-        NSString* messageString = [NSString stringWithFormat: @"Something went wrong. Please contact us.\nDebug: posix_spawn exit code: \"%i", gafExitCode];
+        NSString* messageString = [NSString stringWithFormat: @"Something went wrong. Please contact us.\nDebug: posix_spawn exit code: \"%i\"", gafExitCode];
         UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error" message:messageString preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){}]; // Dismiss button
         UIAlertAction* contactAction = [UIAlertAction actionWithTitle:@"Contact" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
@@ -92,10 +92,50 @@
 }
 // Check 4 updates
 - (IBAction)updateButton:(id)sender {
-    NSString *instVersion;
-    NSString *onlineVersion;
+    NSString *onlineVersion; //Version from Packages file + online repo
+    
+    //get the installed version and bundle identifier from the file located in /etc/gandalf/properties
+      // Check if file exists
+    NSFileManager *fileManagerCheckProp = [NSFileManager defaultManager];
+    if (![fileManagerCheckProp isReadableFileAtPath:@"/etc/gandalf/properties"]){
+        NSLog(@"ERROR: /etc/gandalf/properties is not readable or does not exist.");
+        
+        UIAlertController* propertiesNoExistAlert = [UIAlertController alertControllerWithTitle:@"File doesn´t exist or not readable"
+                                                                       message:@"Couldn´t read file \"/etc/gandalf/properties\".\n Does this file exist, do you have removed Gandalf or is this app running in a sandbox?\nIf you think this is an error, please contact us."
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){}]; // Dismiss button
+        UIAlertAction* contactAction = [UIAlertAction actionWithTitle:@"Contact" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
+                                        {
+                                            NSURL *contactUrl = [NSURL URLWithString:@"https://github.com/ethanrdoesmc/gandalf/issues"];
+                                            if (![[UIApplication sharedApplication] openURL:contactUrl]) {
+                                                NSLog(@"%@%@",@"Failed to open url:",[contactUrl description]); // If failed goes here
+                                            }
+                                        }]; //Contact Button; Opens issues tab on GitHub repo
+        
+        [propertiesNoExistAlert addAction:defaultAction];
+        [propertiesNoExistAlert addAction:contactAction];
+        [self presentViewController:propertiesNoExistAlert animated:YES completion:nil];
+        return; // quit IBAction because we can´t get content of file /etc/gandalf/properties
+
+        
+        
+    }
+    
+    NSString *propertiesFileContent = [NSString stringWithContentsOfFile:@"/etc/gandalf/properties" encoding:NSUTF8StringEncoding error:nil]; //Load content of this file into a string
+    NSArray *propertiesFileArray = [propertiesFileContent componentsSeparatedByString:@"\n"]; // and now into array so we can work with the lines of the file
+    
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // IMPORTANT: THE FOLLOWING WILL FAIL IF YOU CHANGE THE FORMAT OF THE PROPERTIES FILE !!//
+    //////////////////////////////////////////////////////////////////////////////////////////
+    
+    NSString *gandalfIdentifier = [propertiesFileArray objectAtIndex:0]; //get the content of first line (at array index 0) and save it into gandalfIdentifier variable
+    NSString *instVersion = [propertiesFileArray objectAtIndex:1]; //get the content of the second line (at array index 1) and save it into instVersion variable
+    gandalfIdentifier = [gandalfIdentifier stringByReplacingOccurrencesOfString:@"BUNDLEID: " withString:@""]; // get rid of "BUNDLEID: "
+    NSLog(@"Identifier: %@", gandalfIdentifier); // output identifier to the log
+    instVersion = [instVersion stringByReplacingOccurrencesOfString:@"BUILD: " withString:@""];
+    NSLog(@"InstalledVersion: %@", instVersion); // output version number to the log
     __block NSString *packagesFile = @"//EMPTY/";
-    NSString *gandalfIdentifier = @"io.github.ethanrdoesmc.gandalf102"; //Bundle Identifier
+    
     // Get latest version from repository
     
     NSString *Url = @"https://ethanrdoesmc.github.io/gandalf/Packages";
@@ -119,66 +159,18 @@
             break;
         }
     } // wait till block is finished
-    NSLog(@"GG: %@", packagesFile);
+    NSLog(@"Content DL:\n%@", packagesFile);
     NSArray *packagesFileArray = [packagesFile componentsSeparatedByString:@"\n"]; // Save it in an array
-    NSString *rawVersion;
     
     // get all the properties of the gandalf Version from the array
     NSString *packageLine = [NSString stringWithFormat:@"Package: %@", gandalfIdentifier]; // merge the 2 strings
     NSUInteger indexOfGandalfBundleIdentifier = [packagesFileArray indexOfObject:packageLine]; //get the index of them
     NSUInteger arrayCounter = indexOfGandalfBundleIdentifier; //Set where it should start searching
-    NSLog(@"Line: %lu", (unsigned long)indexOfGandalfBundleIdentifier); //output it to the log
-    do {
-        NSString *arrayLine = packagesFileArray[arrayCounter]; // Save content of array line...
-        if ([arrayLine containsString:(@"Version:")]){ //And check if it contains Version:
-            NSLog(@"L: %@", arrayLine);
-            rawVersion = [arrayLine stringByReplacingOccurrencesOfString:@"Version: " withString:@""]; // Remove Version: from this string
-            onlineVersion = rawVersion; //save it to onlineVersion
-            NSLog(@"%@", rawVersion);
-            break; //Now it´s done, so we can stop the loop
-        }
-        arrayCounter++;
-    }
-    while (arrayCounter < 20000000); // Hangs in while loop!!!!!
-    
-    // let´s get the content of /var/lib/dpkg/status and search for gandalfIdentifier
-    // NSString *filepath = [[NSBundle mainBundle] pathForResource:@"/var/lib/dpkg/" ofType:@"status"];
-    NSError *errordpkg;
-    NSString *fileContents = [NSString stringWithContentsOfFile:@"/var/lib/dpkg/status" encoding:NSUTF8StringEncoding error:&errordpkg];
-    
-    if (errordpkg)
-        NSLog(@"Error reading file: %@", errordpkg.localizedDescription);
-    
-    // maybe for debugging...
-    NSLog(@"contents: %@", fileContents);
-    NSArray *dpkgStatusArray = [fileContents componentsSeparatedByString:@"\n"];
-    
-    packageLine = [NSString stringWithFormat:@"Package: %@", gandalfIdentifier]; // merge the 2 strings
-    NSUInteger dpkgIndexOfGandalfBundleIdentifier = [dpkgStatusArray indexOfObject:packageLine]; //get the index of them
-    arrayCounter = dpkgIndexOfGandalfBundleIdentifier; //Set where it should start searching
-    do {
-        NSString *arrayLine = dpkgStatusArray[arrayCounter]; // Save content of array line...
-        if ([arrayLine containsString:(@"Version:")]){ //And check if it contains Version:
-            NSLog(@"L: %@", arrayLine);
-            rawVersion = [arrayLine stringByReplacingOccurrencesOfString:@"Version: " withString:@""]; // Remove Version: from this string
-            instVersion = rawVersion; //save it to instVersion
-            NSLog(@"%@", rawVersion);
-            break; //Now it´s done, so we can stop the loop
-        }
-        arrayCounter++;
-    } while (arrayCounter < 20000000);
-    instVersion = [instVersion stringByReplacingOccurrencesOfString:@"." withString:@""]; // Remove all "." from this string
-    onlineVersion = [onlineVersion stringByReplacingOccurrencesOfString:@"." withString:@""]; // Remove all "." from this string
-    NSUInteger instVersionInt, onlineVersionInt;
-    instVersionInt = [instVersion intValue];
-    onlineVersionInt = [onlineVersion intValue];
-    NSLog(@"ii: %lu", (unsigned long)instVersionInt);
-    NSLog(@"oi: %lu", (unsigned long)onlineVersionInt);
-    if (instVersionInt == 0 || onlineVersionInt == 0){ //Check if string to int worked
-        NSLog(@"Either instVersionInt or onlineVersionInt is invalid!");
-        
-        NSString* messageString = [NSString stringWithFormat: @"Something went wrong. Please contact us. \nDebug: instVersionInt: %lui onlineVersionInt: %lui", (unsigned long)instVersionInt, (unsigned long)onlineVersionInt];
-        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error" message:messageString preferredStyle:UIAlertControllerStyleAlert];
+    if (indexOfGandalfBundleIdentifier > [packagesFileArray count]) { // get here if index of bundleidentifier is greater than the actual array. this can happen if the bundleidentifier in the properties file is not found online.
+        NSLog(@"Something went wrong. Maybe we couldn´t find your bundle identifier online? Your properties file might be corrupted.");
+        UIAlertController* indexToBigAlert = [UIAlertController alertControllerWithTitle:@"Error"
+                                                                                                                                                                                                            message:@"Something went wrong. Maybe we couldn´t find your bundle identifier online? Your properties file might be corrupted. Please contact us, and check the content after \"BUNDLEID:\" in the file \"/etc/gandalf/properties\"\nDebug: \"indexOfBundleIdentifier > packagesFileArray count\""
+                                                                                                                                                                                                                 preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){}]; // Dismiss button
         UIAlertAction* contactAction = [UIAlertAction actionWithTitle:@"Contact" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
                                         {
@@ -188,14 +180,32 @@
                                             }
                                         }]; //Contact Button; Opens issues tab on GitHub repo
         
-        [alert addAction:defaultAction];
-        [alert addAction:contactAction];
-        [self presentViewController:alert animated:YES completion:nil]; // Show message if fails
+        [indexToBigAlert addAction:defaultAction];
+        [indexToBigAlert addAction:contactAction];
+        [self presentViewController:indexToBigAlert animated:YES completion:nil];
+
+        
+        
+        return;
+        
     }
-    if (instVersionInt < onlineVersionInt){ //if there´s an update show a message...
+    NSLog(@"Line: %lu", (unsigned long)indexOfGandalfBundleIdentifier); //output it to the log
+    do {
+        NSString *arrayLine = packagesFileArray[arrayCounter]; // Save content of array line...
+        if ([arrayLine containsString:(@"Version:")]){ //And check if it contains Version:
+            NSLog(@"L: %@", arrayLine);
+            onlineVersion = [arrayLine stringByReplacingOccurrencesOfString:@"Version: " withString:@""]; // Remove Version: from this string
+            NSLog(@"%@", onlineVersion);
+            break; //Now it´s done, so we can stop the loop
+        }
+        arrayCounter++;
+    }
+    while (arrayCounter < 20000000);
+    
+    if ([instVersion compare:onlineVersion options:NSNumericSearch] == NSOrderedAscending){ //if there´s an update show a message... // Like https://stackoverflow.com/questions/1978456/compare-version-numbers-in-objective-c#1990854
         NSLog(@"Update available");
         
-        NSString* messageString = [NSString stringWithFormat: @"There´s an update available.\n\nYou have installed version: %@\nAnd the newest version is: %@", instVersion, onlineVersion];
+        NSString* messageString = [NSString stringWithFormat: @"You have installed version: %@\nand the latest version is: %@", instVersion, onlineVersion];
         UIAlertController* updateAlert = [UIAlertController alertControllerWithTitle:@"Update available" message:messageString preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){}]; // Dismiss button
         UIAlertAction* contactAction = [UIAlertAction actionWithTitle:@"Open Cydia" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
@@ -213,7 +223,7 @@
     } else {
         NSLog(@"No update available");
         
-        NSString* messageString = [NSString stringWithFormat: @"There´s no update available.\n\nYou have installed version: %@\nAnd the newest version is: %@", instVersion, onlineVersion];
+        NSString* messageString = [NSString stringWithFormat: @"You have installed version: %@\nand the latest version is: %@", instVersion, onlineVersion];
         UIAlertController* updateAlert = [UIAlertController alertControllerWithTitle:@"No update available" message:messageString preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){}]; // Dismiss button
         UIAlertAction* contactAction = [UIAlertAction actionWithTitle:@"Open Cydia" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action)
@@ -228,10 +238,5 @@
         [updateAlert addAction:contactAction];
         [self presentViewController:updateAlert animated:YES completion:nil]; // Show message if update available
     }
-    
-    
 }
-
-
-
 @end
